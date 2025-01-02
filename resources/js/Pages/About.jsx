66576b76a5db510ab4956fb1f0fdc2c7844c2ab3
Gabriel <gabriel-vich02@hotmail.com>
2025-01-02 13:32:38 +0100

    added img on about

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -15,6 +15,7 @@ export default function About({ auth}) {
                         <p>Cumque delectus quam vero voluptatum voluptatibus ipsam repellat quasi libero ut omnis, dicta unde corporis nobis at, laboriosam quos sed accusantium eum! Iste aliquam porro totam magni amet placeat voluptate.</p>
                         <p>Cum incidunt fugiat adipisci consequuntur quis! Veritatis praesentium beatae, eligendi distinctio porro labore! Placeat vero quidem illum dolorum minima commodi eveniet delectus modi et. Labore sed et impedit nihil eum.</p>
                         <p>Officia eligendi corporis commodi, aperiam ea id sapiente consectetur fugit vel, itaque illo aut, labore eius natus doloremque repellendus consequuntur possimus aspernatur provident omnis unde? Error alias cumque deserunt. Praesentium.</p>
+                        <img src="assets/img/1.jpg" alt="example"/>
                         <p>Consectetur obcaecati hic iusto quam, aspernatur corporis assumenda nobis consequatur ad architecto voluptas adipisci cupiditate doloribus iure accusantium dicta perferendis incidunt a perspiciatis quo molestias ratione veritatis debitis! Pariatur, voluptatibus!</p>
                     </main>
 
