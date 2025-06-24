41148b04465d09c5d385410ced81d1799eedff34
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2025-06-24 13:34:37 +0200

    production bug fix

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,41 +7,56 @@ use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-   public function index(Request $request)
-{
-    $query = Group::query();
+    public function index(Request $request)
+    {
+        $query = Group::query();
 
-    // Filtro de búsqueda por título
-    if ($request->filled('search')) {
-        $query->where('groups.title', 'like', '%' . $request->input('search') . '%');
-    }
+        // Filtro de búsqueda por título
+        if ($request->filled('search')) {
+            $query->where('groups.title', 'like', '%' . $request->input('search') . '%');
+        }
 
-    $sort = $request->input('sort');
+        $sort = $request->input('sort');
 
-    if ($sort === 'popular') {
-        $query->withCount('notes')
-              ->orderByDesc('notes_count');
-    } elseif ($request->filled(['lat', 'lng'])) {
-        $lat = $request->input('lat');
-        $lng = $request->input('lng');
+        if ($sort === 'popular') {
+            $query->withCount('notes')
+                ->orderByDesc('notes_count');
+        } elseif ($request->filled(['lat', 'lng'])) {
+            $lat = $request->input('lat');
+            $lng = $request->input('lng');
 
-        $query->selectRaw('groups.id, groups.uid, groups.title, groups.latitude, groups.longitude, groups.area, groups.district, groups.locality, groups.street_address, groups.postal_code, groups.link, groups.price, groups.created_at, groups.updated_at, COUNT(notes.id) as notes_count,
+            $query->selectRaw('groups.id, groups.uid, groups.title, groups.latitude, groups.longitude, groups.area, groups.district, groups.locality, groups.street_address, groups.postal_code, groups.link, groups.price, groups.created_at, groups.updated_at, COUNT(notes.id) as notes_count,
             (6371 * acos(
                 cos(radians(?)) * cos(radians(groups.latitude)) *
                 cos(radians(groups.longitude) - radians(?)) +
                 sin(radians(?)) * sin(radians(groups.latitude))
             )) AS distance', [$lat, $lng, $lat])
-            ->leftJoin('notes', 'groups.id', '=', 'notes.group_id')
-            ->groupBy('groups.id')
-            ->orderBy('distance');
-    } else {
-        $query->withCount('notes');
+                ->leftJoin('notes', 'groups.id', '=', 'notes.group_id')
+                ->groupBy([
+                    'groups.id',
+                    'groups.uid',
+                    'groups.title',
+                    'groups.latitude',
+                    'groups.longitude',
+                    'groups.area',
+                    'groups.district',
+                    'groups.locality',
+                    'groups.street_address',
+                    'groups.postal_code',
+                    'groups.link',
+                    'groups.price',
+                    'groups.created_at',
+                    'groups.updated_at'
+                ])
+                ->orderBy('distance');
+        } else {
+            $query->withCount('notes');
+        }
+
+        $groups = $query->get();
+
+        return view('group.index', compact('groups'));
     }
-
-    $groups = $query->get();
-
-    return view('group.index', compact('groups'));
-}
 
 
 
